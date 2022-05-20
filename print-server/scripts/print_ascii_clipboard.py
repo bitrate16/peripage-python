0007c908f90cbd5bc393121fd606b78b4a29bdd2
Eytlin <bitrate16@gmail.com>
2022-05-20 21:28:28 +0300

    Add prefilter for ASCII to avoid POST encoding exception

--- print-server/scripts/print_ascii_clipboard.py
+++ print-server/scripts/print_ascii_clipboard.py
@@ -7,6 +7,7 @@ BREAK         = 1 # Enable/disable
 CONCENTRATION = 2 # Value (0-2)
 
 s = pyperclip.paste().strip().replace('\t', '    ')
+s = ''.join([i for i in s if (31 < ord(i) or ord(i) == 10) and ord(i) < 127])
 print(s)
 
 r = requests.post(
